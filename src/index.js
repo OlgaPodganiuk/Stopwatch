f855af703f0ba6b8ea693fcb877d7dfb3a249711
OlgaPodganiuk <olga79666@gmail.com>
2022-01-28 18:20:18 +0200

    stopwatch

--- src/index.js
+++ src/index.js
@@ -1,17 +1,67 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import $ from 'jquery';
+import { interval, Subject, fromEvent } from "rxjs";
+import { takeUntil, repeatWhen, share, filter } from "rxjs/operators";
+import './index.css'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const action$ = new Subject();
+const stop$ = action$.pipe(filter(action => action === 'stop'));
+const reset$ = action$.pipe(filter(action => action === 'reset'));
+
+const observable$ = interval(1000)
+  .pipe(share())
+  .pipe(
+  takeUntil(stop$),
+  repeatWhen(() => reset$)
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const App = () => {
+  const [time, setTime] = useState(0);
+  const [timeOn, setTimeOn] = useState(true);
+ 
+  const startTime = () => {
+    const start = $('#start');
+    const startTime$ = fromEvent(start, 'click');
+
+    startTime$.subscribe(() => observable$.subscribe(setTime));
+  }
+
+  const stop = () => {
+    setTimeOn(true);
+    setTime(0);
+    action$.next('stop')
+  };
+
+  const reset = () => {
+    setTimeOn(true);
+    setTime(0);
+    action$.next('stop')
+    action$.next('reset')
+  };
+
+  const wait = () => {
+    setTimeOn(!timeOn);
+    if (!timeOn) {
+      action$.next('stop')
+    } 
+  };
+
+  return (
+    <div className='container'>
+      <div className='stopwatch'>
+        <span>{('0' + Math.floor((time / 3600) % 60)).slice(-2)}</span>:
+        <span>{('0' + Math.floor((time / 60) % 60)).slice(-2)}</span>:
+        <span>{('0' + (time % 60)).slice(-2)}</span>
+      </div>
+      <div className='btns'>
+        <button id='start' onClick={startTime}>Start</button>
+        <button onClick={stop}>Stop</button>
+        <button onClick={reset}>Reset</button>
+        <button onClick={wait}>Wait</button>
+      </div>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
